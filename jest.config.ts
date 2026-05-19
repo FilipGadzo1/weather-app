bd3b53331dd09b6042ee42a6881ea618437dfafc
FilipGadzo1 <filip.gadzo123@gmail.com>
2026-05-19 20:50:32 +0200

    fix: use CommonJS syntax in jest.config and add test script

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest'
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({ dir: './' })
 
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
@@ -11,4 +11,4 @@ const config: Config = {
   },
 }
 
-export default createJestConfig(config)
+module.exports = createJestConfig(config)
